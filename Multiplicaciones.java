9197ca6de41795017b63cce1b934c3415a05b1a1
rusoloco <65031127+rusoloco@users.noreply.github.com>
2020-05-22 19:52:17 +0200

    Add files via upload

---
+++ Multiplicaciones.java
@@ -0,0 +1,28 @@
+package operaciones;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Multiplicaciones {
+
+	@Test
+	void test() {
+		Multiplicaciones multiplicacion = new Multiplicaciones();
+		int a = 1;
+		int b = 2;
+		int actual = multiplicacion.add(a, b);
+
+
+
+
+
+
+	}
+
+	private int add(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ multiplicacion.java
@@ -0,0 +1,10 @@
+package operaciones;
+
+import java.util.Scanner;
+
+public class multiplicacion {
+
+	public int add(int a, int b) {
+		return a * b;
+	}
+}
